cd36e798923a3a0ae1f7c87ac77e4233ce4c30c0
oper-V <igor.prohorenko@gmail.com>
2021-02-14 12:59:56 +0200

    Модуль 0. Урок 3. Создание графа игры. Тесты пройдены

--- src/ThreadHelper.java
+++ src/ThreadHelper.java
@@ -2,6 +2,8 @@ public class ThreadHelper {
 
     public static void startAndJoinThread(final Thread t) throws Exception {
         // BEGIN (write your solution here)
+        t.start();
+        t.join();
 
 
         // END
