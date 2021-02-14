7a094bbdb3ed7b229eafd9bdc96398cdf82bcf74
oper-V <igor.prohorenko@gmail.com>
2021-02-14 15:43:33 +0200

    Переход к новому заданию. Модуль 1. Урок 0. Многопоточное построение дерева состояний игры. Задание решено в простом варианте, с одним потоком.

--- src/ThreadHelper.java
+++ src/ThreadHelper.java
@@ -15,6 +15,41 @@ public class ThreadHelper {
 
     private static class FibCalculator extends Thread {
         // BEGIN (write your solution here)
+        // Поле, которое хранит порядковый номер числа Фибоначчи в текущем объекте.
+        int currentNum;
+        // Поле, которое хранит в себе результат вычисления в текущем объект
+        int result;
+        int lastItem; // = 1
+        int beforeLastItem; //= 0
+
+
+
+        public FibCalculator(final int numberToCalculate) {
+            this.currentNum = numberToCalculate;
+            this.lastItem = 1;
+
+        }
+
+        @Override
+        public void run() {
+            if(currentNum == 1) {
+                result = 1;
+            }
+            else {
+                //result = 0;
+                for(int i = 1; i < currentNum; i++) {
+                    result = lastItem + beforeLastItem;
+                    beforeLastItem = lastItem;
+                    lastItem = result;
+                }
+            }
+
+        }
+
+        public int getResult() {
+            return result;
+        }
+
 
         // END
     }
